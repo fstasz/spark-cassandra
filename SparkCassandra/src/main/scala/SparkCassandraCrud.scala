c184fcf41d32183c2273bbd21d2cafd6ad8fa2b9
Frank Staszak <fs4364@gmail.com>
2015-07-05 10:27:47 -0700

    Formatting changes to SparkCassandraCrud.scala.

--- SparkCassandra/src/main/scala/SparkCassandraCrud.scala
+++ SparkCassandra/src/main/scala/SparkCassandraCrud.scala
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import org.apache.spark.{ SparkConf, SparkContext }
 import com.datastax.spark.connector._
 
@@ -13,9 +32,9 @@ object SparkCassandraCrud {
       .setMaster(Configuration.SparkMaster)
       
     /** Create SparkContext */  
-    val sc = new SparkContext(Configuration.ThreadConfig, Configuration.AppName, conf)
+    val sc = new SparkContext(Configuration.ClusterUrl, Configuration.AppName, conf)
     
-    /** Read text from words file */
+    /** Read lines from words file */
     val wordList = sc.textFile(Configuration.WordFile, 2).cache()
     
     /** Return a new RDD of words containing SearchString */
@@ -37,7 +56,7 @@ object SparkCassandraCrud {
   object Configuration {
     val AppName = "CassandraSpark"
     val SparkMaster = "spark://localhost:7077"
-    val ThreadConfig = "local[2]"
+    val ClusterUrl = "local[2]"
     val CassandraConnection = "spark.cassandra.connection.host"
     val CassandraNode = "localhost"
     val CassandraKeyspace = "test"
